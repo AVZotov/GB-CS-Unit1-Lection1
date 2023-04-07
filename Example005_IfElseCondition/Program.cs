337af0c01c9fc428e4c66629e683feb0efaa1f6d
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-07 11:04:54 +0300

    Example005 Conditions added

---
+++ Example005_IfElseCondition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+if(name == "Masha")
+{
+    Console.WriteLine("Yehoo, Mariya!!!");
+}
+else
+{
+    Console.WriteLine("Hello " + name);
+}
