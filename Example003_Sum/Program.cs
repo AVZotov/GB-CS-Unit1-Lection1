6926ce2470aa08c46beb65866518a41004d348a7
Alexey_Zotov <Alexey.Zotov@ymail.com>
2023-04-07 10:55:42 +0300

    random generator added

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
 
+Console.Write("number A: " + numberA + " numberB: " + numberB + "\n");
 Console.WriteLine(numberA + numberB);
-
